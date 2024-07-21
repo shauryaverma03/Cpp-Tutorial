f1e014921b89bec53c75d5665b2c9fef3d94a34b
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-07-22 00:47:54 +0530

    Update butterfly.cpp

--- butterfly.cpp
+++ butterfly.cpp
@@ -10,10 +10,10 @@ int main() {
     for (int j=1; j<=i; j++) {
       cout<<"* ";
     }
-    for (int k=1; k<(n*i)*2; k++) {
+    for (int k=1; k<(n-i)*2; k++) {
       cout<<"  ";
     }
-    for (int i=1; i<=i; i++) {
+    for (int l=1; l<=i; l++) {
       cout<<"* ";
     }
     cout<<endl;
@@ -23,10 +23,10 @@ int main() {
     for (int j=1; j<=i; j++) {
       cout<<"* ";
     }
-    for (int k=1; k<(n*i)*2; k++) {
+    for (int k=1; k<(n-i)*2; k++) {
       cout<<"  ";
     }
-    for (int i=1; i<=i; i++) {
+    for (int l=1; l<=i; l++) {
       cout<<"* ";
     }
     cout<<endl;
