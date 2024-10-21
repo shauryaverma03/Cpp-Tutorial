e58b1adde24c41451065b484c19dc79bf944abf8
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-10-21 13:34:52 +0530

    Create question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,5 @@
+Early Closure Charges are usually applicable when you want to repay the entire outstanding loan amount in one single payment.
+Early Closure Charges are 5% of the Principal Outstanding.
+Part Payment Charges are usually applicable when you want to repay a part of outstanding loan amount.
+Part Prepayment charges are 5% of the Part Payment Amount (Part Payment will be allowed twice in a loan calendar year and once in a loan calendar month. The minimum amount accepted for part payment will be equal to one EMI. Maximum amount allowed for part payment will be 25% of the balance principal in one year.)
+Documentation Charges & Processing Fee is applicable for processing your two wheeler loan. Documentation charges are 
