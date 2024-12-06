199fbe631ef7fad1c9aff69b1a184e196f640e0f
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-12-07 00:40:57 +0530

    Create class_and_object.cpp

---
+++ class_and_object.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+class Class {
+  public:
+    void output() {
+      cout<<"Class and Object";
+    }
+
+};
+
+int main() {
+  Class object;
+  object.output();
+  return 0;
+}
