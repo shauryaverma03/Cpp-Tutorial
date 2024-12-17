ff14cfe50125a7e9b7bc4125a0469897f9107358
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-12-18 01:24:41 +0530

    Create Algorithm_Sort.cpp

---
+++ Algorithm_Sort.cpp
@@ -0,0 +1,24 @@
+// Sorting using Algorithm 
+
+# include <iostream>
+# include <algorithm>
+using namespace std;
+
+int main() {
+  int n;
+  cout<<"Enter the length of array: ";
+  cin>n;
+
+  int arr[n];
+  fot (int i=0; i<n; i++) {
+    cin>>arr[i];
+  }
+
+  sort(arr,arr+n);
+
+  for (int i=0; i<n; i++) {
+    cout<<arr[i]<<" ";
+  }
+
+  return 0;
+}
